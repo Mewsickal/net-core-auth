177e5b2153ebda6969237c185811b92182132311
Milda <laiminga.milda@gmail.com>
2020-10-05 14:47:14 +0300

    Add identity server

---
+++ IdentityServer/Config.cs
@@ -0,0 +1,33 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("api1", "Api")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client_id",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    ClientSecrets =
+                    {
+                        new Secret("client_secret".Sha256())
+                    },
+                    AllowedScopes = {"api1"}
+                }
+            };
+    }
+}

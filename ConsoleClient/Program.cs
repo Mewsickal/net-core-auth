d9c99b5d67fc0e5118dbe8d9c25f7783f828ff57
Milda <laiminga.milda@gmail.com>
2020-10-05 15:23:55 +0300

    Add console client

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,56 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var client = new HttpClient();
+
+            var discovery = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+
+            if (discovery.IsError)
+            {
+                Console.WriteLine(discovery.Error);
+                return;
+            }
+
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = discovery.TokenEndpoint,
+                ClientId = "client_id",
+                ClientSecret = "client_secret",
+                Scope = "api1"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var apiResponse = await apiClient.GetAsync("https://localhost:6001");
+
+            if (!apiResponse.IsSuccessStatusCode)
+            {
+                Console.WriteLine(apiResponse.StatusCode);
+            }
+            else
+            {
+                var content = await apiResponse.Content.ReadAsStringAsync();
+                Console.WriteLine(content);
+            }
+        }
+    }
+}
